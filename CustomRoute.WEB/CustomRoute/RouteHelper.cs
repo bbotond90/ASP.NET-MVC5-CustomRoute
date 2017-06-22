0b044f3a0fc35deed1ccbcf0a6f36e1b31ed96c6
Botond Bertalan <bbotond90@yahoo.com>
2017-06-22 15:07:30 +0100

    doc comments added

--- CustomRoute.WEB/CustomRoute/RouteHelper.cs
+++ CustomRoute.WEB/CustomRoute/RouteHelper.cs
@@ -11,16 +11,18 @@ namespace CustomRoute
     static class RouteHelper
     {
         /// <summary>
-        /// 
+        /// Case insensitive lookup for a view in a specific folder.
         /// </summary>
-        /// <param name="values"></param>
-        /// <param name="valueKey"></param>
-        /// <param name="path"></param>
+        /// <param name="values">IRouteConstraint's RouteValueDictionary</param>
+        /// <param name="valueKey">Case insensitive name of the .cshtml view passed from the route</param>
+        /// <param name="path">Container folder path like: "~/Views/StaticPages"</param>
         /// <returns></returns>
         public static bool IsDestinationViewExists(RouteValueDictionary values, string valueKey, string path)
         {
+            //we have to make sure that we have a view name specified to look for
             if (values[valueKey] == null) return false;
 
+            //get all .cshtml file in the specified path
             List<string> destinationViewList = Directory.GetFiles(
                 System.Web.Hosting.HostingEnvironment.MapPath(path), "*.cshtml")
                 .ToList();
@@ -28,12 +30,17 @@ namespace CustomRoute
             foreach (string destinationPath in destinationViewList)
             {
                 FileInfo destinationInfo = new FileInfo(destinationPath);
+                //remove the extension ".cshtml" from the view name
                 string destination = destinationInfo.Name
                                         .Replace(destinationInfo.Extension, "");
+
+                //case insensitive match
+                //if the file name matches the requested valueKey/view it returns true
                 if (destination.ToLower() == values[valueKey].ToString().ToLower())
                     return true;
             }
 
+            //if we couldn't find a matching view
             return false;
         }
     }
